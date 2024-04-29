aa06ffbc6481f478714977c0ebe062cd26d9613e
Marceloolliveira <marcelodeoliveirapereira47@gmail.com>
2024-04-29 13:18:09 -0300

    Alter Label Version 1.0

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'MKS Api CRUD MKS-desenvolvimento-de-sistemas Version 1.0';
   }
 }
